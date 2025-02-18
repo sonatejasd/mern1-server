ad4830490e4fb0aba67cc2ae3dd82483b050f872
sonatejasd <sonatejasd@gmail.com>
2025-02-18 15:09:43 +0530

    added login api

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/login', function(req, res, next) {
+  var {username, password} = req.body.data;
+  if(username === 'admin' && password === 'admin') {
+    res.send({status: 200, message: 'Login success'});
+  }
+  else
+    res.send({status: 401, message: 'Login failed'});
+})
+
 module.exports = router;
